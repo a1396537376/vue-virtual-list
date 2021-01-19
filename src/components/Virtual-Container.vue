0dc1a7f03b47af060ca9bc1f325a422338c8023a
高东飞,Gao Dongfei <1396537376@qq.com>
2021-01-19 22:20:44 +0800

    [dev] 初始化必要参数

--- src/components/Virtual-Container.vue
+++ src/components/Virtual-Container.vue
@@ -4,7 +4,7 @@
     ref="virtual_list"
     :style="{ height: `${viewHeight}px`}">
     <div class="phantom" :style="{ height: `${listHeight}px`}"></div>
-    <div class="view_content">
+    <div class="view_content" :style="{ transform: transformValue }">
       <div
         ref="items"
         class="view_items"
@@ -16,12 +16,22 @@
   </div>
 </template>
 <script>
+/**
+@params scrollTop  滚动高度
+@params listHeight 列表总高度
+@params startIndex 视图渲染的起始索引
+@params endIndex 视图渲染的结束索引
+
+*/
 export default {
     props: {
       // data is required  
       data: { type: Array, required: true },
       // render window height
       viewHeight: { type: Number },
+      bufferScale: { type: Number, default: 0.5 },
+      itemSize: { type: Number, default: 24 },
+      // nomal | dynamic | log
       pageModel: { type: String, default: 'normal' }
     },
     computed: {
@@ -32,10 +42,30 @@ export default {
               throw Error('data is required!');
           }
       },
+      transformValue() {
+        return `translate3d(0,${this.startOffset}px,0)`;
+      },
+      aboveCount() {
+        return Math.min(this.start,this.bufferScale * this.renderCount)
+      },
+      belowCount() {
+        return Math.min(this.data.length - this.end,this.bufferScale * this.renderCount);
+      },
+      renderCount() {
+        return ~~(this.viewHeight / this.itemSize);
+      },
+      renderList() {
+        let start = this.start - this.aboveCount;
+        let end = this.end + this.belowCount;
+        return this.data.slice(start, end);
+      }
     },
     data() {
       return {
-        positions: [],
+        positions: [],  // all item position info
+        startOffset: 0, // view content offset
+        start: 0,
+        end: -1,
       };
     }
 }
