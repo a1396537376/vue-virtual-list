fd3bae6cc03d264d7b4ea96332867a421ce3b6a6
高东飞,Gao Dongfei <1396537376@qq.com>
2021-02-06 22:45:50 +0800

    🦄refactor(components): 二分搜索滚动对应索引

--- src/components/Virtual-Container.vue
+++ src/components/Virtual-Container.vue
@@ -89,6 +89,26 @@ export default {
           }
         });
       },
+      binarySearch(list,value) {
+        let start = 0;
+        let end = list.length - 1;
+        let tempIndex = null;
+        while(start <= end) {
+          let midIndex = ~~((start + end)/2);
+          let midValue = list[midIndex].bottom;
+          if(midValue === value){
+            return midIndex + 1;
+          }else if(midValue < value){
+            start = midIndex + 1;
+          }else if(midValue > value){
+            if(tempIndex === null || tempIndex > midIndex){
+              tempIndex = midIndex;
+            }
+              end = end - 1;
+            }
+          }
+          return tempIndex;
+        },
     },
 }
 </script>
